9e039aabfd2ede8743211e84bbdece220214caf8
小飼慎一 <kogai0121@gmail.com>
2017-03-30 21:10:01 +0900

    set server with iron

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ extern crate iron;
 use iron::prelude::*;
 use iron::status;
 
-fn main() {
-    fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello World!")))
-    }
+fn hello_world(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Hello World!")))
+}
 
+fn main() {
     let _server = Iron::new(hello_world).http("localhost:3000").unwrap();
     println!("On 3000");
 }
